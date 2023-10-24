df38523a58f4c1007706a472f8d10017949412e5
etimbob <oyamavictor247@gmail.com>
2023-10-23 21:57:12 -0700

    My primary goal of hacking was the intellectual curiosity, the seduction of adventure

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,12 +1,10 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * print_chessboard - Entry point
+ * @a: pointers to pieces to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: void 
  */
 int _putchar(char c)
 {
